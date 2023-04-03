5f668e3b8a01c4a5fdca72debf1cf363b354f3f3
StandaB23 <stanislav.becvar@seznam.cz>
2023-04-03 13:28:09 +0200

    uprava 1

--- InsuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/HomeController.cs
+++ InsuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/HomeController.cs
@@ -22,7 +22,10 @@ namespace InsuranceCorp.MVC.Controllers
         {
             return View();
         }
-
+        public IActionResult Hello()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
