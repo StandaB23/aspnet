6d3324fb73227283304567e47be84257cdc5998f
StandaB23 <stanislav.becvar@seznam.cz>
2023-04-03 16:53:02 +0200

    upravy 3

---
+++ InsuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using InsuranceCorp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsuranceCorp.MVC.Controllers
+{
+    public class PersonController : Controller
+    {
+        private InsCorpDbContext _context;
+
+        public PersonController(InsCorpDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            // 1. ziskat data
+            var top100 = _context.Persons.Include(person => person.Constracts).OrderBy(person => person.Id).Take(100).ToList();
+
+            // 2. zobrazit view
+            return View(top100);
+        }
+        public IActionResult Detail(int id)
+        {
+            var person = _context.Persons.Find(id);
+
+            return View(person);
+        }
+    }
+}
--- InsuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/StatusController.cs
+++ InsuranceCorp/InsuranceCorp/InsuranceCorp.MVC/Controllers/StatusController.cs
@@ -1,11 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using InsuranceCorp.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace InsuranceCorp.MVC.Controllers
 {
     public class StatusController : Controller
     {
+        private readonly InsCorpDbContext _context;
+        public StatusController(InsCorpDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            bool ok = _context.Database.CanConnect();
+
+            ViewData["ok"] = ok;
+
             return View();
         }
     }
